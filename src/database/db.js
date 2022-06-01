b6d4cacd2e90098acc0d3be4df11cec44246f396
marcos <76123467+markalCuest@users.noreply.github.com>
2022-06-01 13:36:13 +0200

    deplpy

--- src/database/db.js
+++ src/database/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 //Establecemos los parametros de conexion a la base de datos
 var conexion = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'reservas_padel'
+	host: 'mysql-marcoscuesta.alwaysdata.net',
+	user: '271383',
+	password: 'CursoDaw.9',
+	database: 'marcoscuesta_reservas_padel'
 });
 
 module.exports = conexion;
